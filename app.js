5a39da6af9e4745b42f10ec21f3dc9dbb69dd21b
Ankit <ankitraj1307@outlook.com>
2024-10-09 21:50:20 +0530

    responsive changes

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.get("*", (req, res) => {
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT ;
+const port = process.env.PORT || 5000;
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
